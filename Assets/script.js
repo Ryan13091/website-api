8bfd55bc688e09dabb5404f7993319d99a3f2a91
Ryan13091 <65370553+Ryan13091@users.noreply.github.com>
2020-06-15 20:27:33 -0500

    added basic variables to JS

---
+++ Assets/script.js
@@ -0,0 +1,7 @@
+//Start Creating variables needed on page
+var page = document.getElementById('page-body')
+var pageContent = document.getElementById('page-content')
+var highScoresPage = document.getElementById('high-scores')
+var startQuizBtn = document.getElementById('starts-quiz')
+var timeDisplay = document.getElementById('timer')
+
