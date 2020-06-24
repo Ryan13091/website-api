d9c22b61cb06b38906cd80a33e21520a7870daff
Ryan13091 <65370553+Ryan13091@users.noreply.github.com>
2020-06-23 22:51:08 -0500

    cleaned up all code, now all functioning properly.

--- Assets/script.js
+++ Assets/script.js
@@ -1,13 +1,13 @@
 //Start Creating variables needed on page for index.html to pull from
-var page = document.getElementById('page-body')
-var pageContent = document.getElementById('page-content')
-var highScoresTxtLink = document.getElementById('high-scores')
-var startTheQuizBtn = document.getElementById('starts-quiz')
-var timeDisplay = document.getElementById('timer')
+var page = document.getElementById('page-body');
+var pageContent = document.getElementById('page-content');
+var highScoresTxtLink = document.getElementById('high-scores');
+var startTheQuizBtn = document.getElementById('starts-quiz');
+var timeDisplay = document.getElementById('timer');
 
 //Start - Creating Global Variable Needed in Console etc. to call out later down the page
 var counter = 60;
-var scoreArray = loadNewScore() || [];
+var scoreArray = loadScores() || [];
 var questIndex = 0;
 //Starts loading the questions for the quiz
 var questArray = [
@@ -41,7 +41,7 @@ var questArray = [
 ];
 
 // Need Event Listeners to create clickable buttons that trigger a callback from Variables
-highScoresTxtLink.addEventListener('click', highScoresPage);
+highScoresTxtLink.addEventListener('click', topScoresPage);
 startTheQuizBtn.addEventListener('click', startQuiz);
 
 //Define call back functions intentions
@@ -57,7 +57,7 @@ function timer() {
     window.startCount = setInterval(function () {
         counter--;
         //counts down by 1 - now definie that it should count down to zero and display
-        counter > 0 ? (timerDisplay.innerText = counter) : (timeDisplay.innerText = 0);
+        counter > 0 ? (timeDisplay.innerText = counter) : (timeDisplay.innerText = 0);
         if (counter === 0) {
             clearInterval(startCount);
             gameFinishWindow();
@@ -74,42 +74,44 @@ function createQuestion(questionObj) {
     questIndex++;
 
     //create question elements on page-content container
-    var questContainerEl = document.createElement('div');
-    questContainerEl.className = 'question';
-    questContainerEl.id = 'question';
+    var questionContainerEl = document.createElement('div');
+    questionContainerEl.className = 'question';
+    questionContainerEl.id = 'question';
 
     //push questions within container
     var questEl = document.createElement('h2');
     questEl.textContent = questionObj.question;
+
     //add the question as a child with append function
-    questContainerEl.appendChild(questEl)
+    questionContainerEl.appendChild(questEl);
     //show the available answers
     var answersAvailEl = document.createElement('ul');
     answersAvailEl.className = 'answer-avail-list';
-    answersAvailEl.id = 'answer-avail-list'
+    answersAvailEl.id = 'answer-avail-list';
 
     //pull the available answers list on front end
     var answers = questionObj.answers;
     //need for var - to store answers
-    for (var i = 0; i < answers.length; i++)
-        var answerEl = document.createElement('li')
-    answerEl.className = 'answer-avail-item'
-    // need to create a button to submit answer
-    answerEl.innerHTML = `<button class="btn answer-btn">${i + 1}. ${answers[i]}</button>`;
-    //let user know if answer was correct or not
-    if (i === questionObj.correctAnswersIndex) {
-        answerEl.setAttribute('answer-correct', 'true');
+    for (var i = 0; i < answers.length; i++) {
+        var answerEl = document.createElement('li');
+        answerEl.className = 'answer-avail-item';
+        // need to create a button to submit answer
+        answerEl.innerHTML = `<button class="btn answer-btn">${i + 1}. ${answers[i]}</button>`;
+        //let user know if answer was correct or not
+        if (i === questionObj.correctAnswersIndex) {
+            answerEl.setAttribute('answer-correct', 'true');
+        }
+        // place answer 
+        answersAvailEl.appendChild(answerEl);
     }
-    // place answer 
-    answerAvailListEl.appendChild(answerEl);
+
+    questionContainerEl.appendChild(answersAvailEl);
+    pageContent.appendChild(questionContainerEl);
+
+    //add event listener for user when they click on Answer object
+    answersAvailEl.addEventListener('click', verifyAnswer);
 }
-
-questContainerEl.appendChild(answerAvailListEl);
-pageContent.appendChild(questContainerEl);
-
-//add event listener for user when they click on Answer object
-answerAvailListEl.addEventListener('click', verifyAnswer);
-
+//check to see if answer from user is correct and change variables accordingly
 function verifyAnswer(event) {
     var clicked = event.target.closest('li.answer-avail-item');
     var answerList = document.getElementById('answer-avail-list');
@@ -119,14 +121,14 @@ function verifyAnswer(event) {
         var isCorrectAnswer = clicked.hasAttribute('answer-correct');
         //now tell user they were right
         if (isCorrectAnswer) {
-            var rightMsgEl = document.createElement('p')
+            var rightMsgEl = document.createElement('p');
             rightMsgEl.className = 'feedback-msg';
             rightMsgEl.innerText = 'Right Answer!';
             answerList.appendChild(rightMsgEl);
             //or tell user they were wrong and deduct 10 seconds from 60
         } else {
             counter = counter - 10;
-            var wrongMsgEl = document.createElement('p')
+            var wrongMsgEl = document.createElement('p');
             wrongMsgEl.className = 'feedback-msg';
             wrongMsgEl.innerText = 'Wrong Answer!';
             answerList.appendChild(wrongMsgEl);
@@ -146,7 +148,7 @@ function verifyAnswer(event) {
 
 //Need function to display final scores and also to clear/go back and stop the Startcount timer
 function endGamePage() {
-    clearInterval(window.startCount)
+    clearInterval(window.startCount);
 
     pageContent.innerHTML = '';
     var endGameEl = document.createElement('div');
@@ -160,8 +162,8 @@ function endGamePage() {
     var finScoreMsgEl = document.createElement('h3');
     finScoreMsgEl.innerText = 'Check your final score: ';
 
-    var finScoreEl = document.createElement('span')
-    finScoreEl.id = 'final=score'
+    var finScoreEl = document.createElement('span');
+    finScoreEl.id = 'final=score';
 
     if (counter >= 0) {
         finScoreEl.innerText = counter + '.';
@@ -188,7 +190,7 @@ function endGamePage() {
 function allStatsSubmit(event) {
     event.preventDefault();
     var userInitials = document.querySelector("input[name='initials']")
-        .nodeValue.toUpperCase();
+        .value.toUpperCase();
     var userScore = counter > 0 ? counter : 0;
     var userStatsObj = {
         player: userInitials,
@@ -198,7 +200,7 @@ function allStatsSubmit(event) {
     saveScores();
 
     //high score page
-    topScoresPage()
+    topScoresPage();
 }
 
 //need fuction to call variable to show all top scores
@@ -206,6 +208,61 @@ function allStatsSubmit(event) {
 function topScoresPage() {
     page.innerHTML = '';
 
-    varTopScoresContainerEl
+    var topScoresContainerEl = document.createElement('div');
+    topScoresContainerEl.classList = 'containter high-scores';
+    topScoresContainerEl.id = 'high-scores';
+
+    var heading = document.createElement('h2');
+    heading.innerText = 'High Scores';
+    topScoresContainerEl.appendChild(heading);
+
+    var highScoresList = document.createElement('ul');
+    for (var i = 0; i < scoreArray.length; i++) {
+        var playerStats = document.createElement('li');
+        playerStats.innerText =
+            i + 1 + '. ' + scoreArray[i].player + ' - ' + scoreArray[i].score;
+        highScoresList.appendChild(playerStats);
+    }
+    topScoresContainerEl.appendChild(highScoresList);
+
+    var actionContainer = document.createElement('div');
+    actionContainer.className = 'actions';
+
+    var goBackBtn = document.createElement('a');
+    goBackBtn.id = 'go-back';
+    goBackBtn.setAttribute('href', './index.html');
+    goBackBtn.classList = 'btn btn-short';
+    goBackBtn.innerText = '<-- Back';
+    actionContainer.appendChild(goBackBtn);
+
+    var clearAllScores = document.createElement('button');
+    clearAllScores.classList = 'btn btn-short';
+    clearAllScores.innerText = 'Clear All Scores';
+    clearAllScores.addEventListener('click', handleClearAllScores);
+    actionContainer.appendChild(clearAllScores);
+
+    topScoresContainerEl.appendChild(actionContainer);
+    page.appendChild(topScoresContainerEl);
 }
 
+//need function to actually clear the scores
+function handleClearAllScores() {
+    scoreArray = [];
+    saveScores();
+    topScoresPage();
+}
+
+//need function to put top scores in order from high to low
+function saveScores() {
+    scoreArray.sort((a, b) => b.score - a.score);
+    localStorage.setItem('stats', JSON.stringify(scoreArray));
+}
+
+// need function to pull highest scores from local storage
+function loadScores() {
+    var stats = localStorage.getItem('stats');
+    if (!stats) {
+        return false;
+    }
+    return (stats = JSON.parse(stats));
+}
